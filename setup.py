1575afbc17c965614f5bcef0470335ac9b049ab8
Sam Carson <37749448+samcarsonx@users.noreply.github.com>
2023-07-04 02:01:11 +0100

    Added dependencies

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author_email="sam@samcarson.co.uk",
     license="None",
     packages=find_packages(),
-    install_requires=[""],
+    install_requires=["asyncio", "websockets", "json", "ctypes", "enum"],
     python_requires=">=3.6",
 )
