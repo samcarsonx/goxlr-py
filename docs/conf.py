dea7c81cdaac71b38d9515d8364a7ee056ffa571
Sam Carson <37749448+samcarsonx@users.noreply.github.com>
2023-07-04 17:41:25 +0100

    Fix relative import for Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,15 @@
 import os
 import sys
 
-# Add your project directory to the Python path
-sys.path.insert(0, os.path.abspath(".."))
-
-from ..version import __version__
+# Add your project root directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 # Project information
 project = "goxlr"
 author = "Sam Carson"
+
+from version import __version__
+
 version = __version__
 release = __version__
 
